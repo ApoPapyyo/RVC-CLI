c341808841b6634639e4346a2c2a21c44c0e4394
Rei Okazawa <reiok0729@gmail.com>
2025-07-02 14:08:48 +0900

    Fix rvc.sh

--- rvc.sh
+++ rvc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 path=$(dirname `realpath "$0"`)
+if ! [ -d "$path/venv" ]; then
+    "$path/setup.sh"
+fi
 . "$path/venv/bin/activate"
 if [ "$weight_root" = '' ]; then
     export weight_root="$path/models/weights"
